e1e9ff9b7732b6d9515da7a309ad82f91912fc64
Kent Jordan <kentaki.dev@gmail.com>
2025-02-06 11:21:19 +0800

    feat: added dynamic data and ui components

--- resources/views/products/view.blade.php
+++ resources/views/products/view.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <script src="https://unpkg.com/lucide@latest"></script>
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
@@ -17,27 +18,47 @@
     </head>
    <body class="flex">
         @include("components.sidebar")
-        <div class="w-full h-screen p-4 pl-4">
-
+        <div class="w-full h-screen p-4 pl-4 select-none">
             <h1 class="text-2xl mt-8 mb-8">List of Products</h1>
-            <div>
-                <table class="w-full border-collapse">
-                    <tr class="flex gap-8 border bg-neutral-100 p-2">
+            <div class="flex justify-between mb-4 items-center">
+                <div class="flex gap-2 p-2 rounded-md items-center border-neutral-300 border ">
+                    <i data-lucide="search" class="text-neutral-500 w-5"></i>
+                    <input type="text" placeholder="Search product" class="focus:outline-none">
+                </div>
+                <div class="flex gap-2 cursor-pointer">
+                    <i data-lucide="plus" class="w-4 text-sm"></i>
+                    <span>New Item</span>
+                </div>
+            </div>
+            <div class="overflow-auto">
+                <table class=" w-full">
+                    <tr class="bg-neutral-100 *:border *:p-2">
+                        <th class="text-left">Actions</th>
                         <th class="text-left">ID</th>
                         <th class="text-left">Name</th>
-                        <th class="text-left">Description</th>
+                        <th class="text-left max-w-16">Description</th>
                         <th class="text-left">Price (PHP)</th>
                         <th class="text-left">Qty</th>
                     </tr>
-                    <tr class="flex gap-8 p-2">
-                        <td class="text-left">01</td>
-                        <td class="text-left">Item 1</td>
-                        <td class="text-left">Lorem ipsum</td>
-                        <td class="text-left">300</td>
-                        <td class="text-left">10</td>
-                    </tr>
+                    @foreach ($product_items as $item)
+                        <tr class="*:border *:p-4">
+                            <td class="text-left">
+                                <div class="flex gap-2">
+                                <i data-lucide="pencil" class="cursor-pointer w-4"></i>
+                                <i data-lucide="trash" class="cursor-pointer w-4 text-red-700"></i>
+                        </div></td>
+                            <td class="text-left">{{$item['id']}}</td>
+                            <td class="text-left">{{$item['name']}}</td>
+                            <td class="text-left max-w-[32ch] text-ellipsis overflow-hidden whitespace-nowrap">{{$item['description']}}</td>
+                            <td class="text-left">{{$item['price']}}</td>
+                            <td class="text-left">{{$item['qty']}}</td>
+                        </tr>
+                    @endforeach
                 </table>
-            </div>
+        </div>
         </div>
    </body>
+   <script>
+        lucide.createIcons();
+    </script>
 </html>
